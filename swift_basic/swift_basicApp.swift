c69e6871a0b5f27db9c9aa771eeb7e6967c8340c
avinashjindal786 <avinashjindal786@gmail.com>
2022-10-02 11:47:33 +0530

    Initial Commit

---
+++ swift_basic/swift_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_basicApp.swift
+//  swift_basic
+//
+//  Created by Avinash jindal on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
