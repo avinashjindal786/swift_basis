803f7ed34e68dcd8a529bf694729f977b4eb2473
avinashjindal786 <avinashjindal786@gmail.com>
2022-10-03 17:24:40 +0530

    first commit

--- swift_basic/ContentView.swift
+++ swift_basic/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Button("Click me", action: {
+                print("Hello World")
+            })
+            Button("Click me", action: {
+                print("Hello World")
+            })
+            Button(action: {}, label: {
+                HStack{
+                           Image(systemName: "pencil")
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                    Text("Edit")
+                }
+            })
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 13 Pro")
+            ContentView()
+                .previewDevice("iPhone 13 Pro")
+        }
     }
 }
